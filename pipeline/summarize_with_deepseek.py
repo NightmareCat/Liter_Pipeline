9710eabc5eb191333eec0cc3c7771b261734c2cc
NightmareCat <lsh94222@yeah.net>
2025-05-16 17:34:48 +0800

    更新格式

--- pipeline/summarize_with_deepseek.py
+++ pipeline/summarize_with_deepseek.py
@@ -4,6 +4,7 @@ from dotenv import load_dotenv
 from openai import OpenAI
 
 MIN_BLOCK_LENGTH = 100
+
 load_dotenv()  # 从.env文件中读取环境变量
 
 def summarize_markdown(md_path: str, config: dict) -> list[str]:
@@ -40,6 +41,7 @@ def summarize_markdown(md_path: str, config: dict) -> list[str]:
     output_dir.mkdir(parents=True, exist_ok=True)
 
     blocks = result_text.split("---") # type: ignore
+    pdf_name = output_dir.name + ".pdf"
     output_paths = []
 
     n_block = 1
@@ -48,12 +50,14 @@ def summarize_markdown(md_path: str, config: dict) -> list[str]:
         if len(block) < MIN_BLOCK_LENGTH:
             # print(f"[跳过] tech_{i+1}.md 内容过短（{len(block)} 字符）")
             continue
-        if not block:
-            continue
+
+        # ✅ 追加“对应文献”信息
+        block += f"\n- 对应文献：{pdf_name}"
+
         out_path = output_dir / f"tech_{n_block}.md"
         with open(out_path, "w", encoding="utf-8") as f:
             f.write(block)
         output_paths.append(str(out_path))
-        n_block +=1
+        n_block += 1
 
     return output_paths
